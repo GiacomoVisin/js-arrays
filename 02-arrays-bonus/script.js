cc5ab8d601505a6ff685422974925c1d56e67e51
GiacomoVisin <killerboss1150@gmail.com>
2026-03-04 14:58:36 +0100

    task bonus n5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -51,4 +51,11 @@ console.log(isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = [];
+
+for (let i = 0; i < teachers.length; i++){
+  teachersString = teachersString + teachers[i] + `,`
+  
+}
+
+console.log(teachersString);
